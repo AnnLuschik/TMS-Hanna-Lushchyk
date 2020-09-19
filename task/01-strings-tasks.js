4698724ad8ccc92639391e3647c2ccd30e1e3159
HP <efafafaef>
2020-09-19 14:19:26 +0300

    add solution for 4 task of 01-strings-tasks.js

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -69,7 +69,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+	return value.slice(0, value.length - 1).split(', ')[1];
 }
 
 
