fd9e651306a806a8b9a2f5caf3cb1ebfa4a227a3
HP <efafafaef>
2020-09-20 00:16:35 +0300

    add solution for 3 task of 03-date-tasks.js

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -56,7 +56,16 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+	let year = date.getFullYear();
+	if(year % 4) {
+		return false;
+	} else if(year % 100) {
+		return true;
+	} else if(year % 400) {
+		return false;
+	} else {
+		return true;
+	}
 }
 
 
