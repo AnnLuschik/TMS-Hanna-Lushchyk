078a4bd58351ddb42fbc3852fe31a65bc30a8690
HP <HP@DESKTOP-8EO1B9C>
2020-11-04 23:10:07 +0300

    feat: add script

--- react-counter-class/src/App.js
+++ react-counter-class/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
+function Counter({ value }) {
+	return (
+		<div className='counter center'>{value}</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [value, setValue] = useState(0);
+
+	return (
+		<div className='container'>
+			<Counter value={value} />
+
+			<div className='buttons-panel'>
+
+				<button className='button increment center' onClick={() => {
+					setValue(value + 1)
+				}}>+</button>
+
+				<button className='button reset center' onClick={() => {
+					setValue(0)
+				}}>&#10227;</button>
+
+				<button className='button decrement center' onClick={() => {
+					setValue(value - 1)
+				}}>–</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
