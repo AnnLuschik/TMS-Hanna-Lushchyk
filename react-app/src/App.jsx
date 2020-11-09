8edc72267108dd0ae662d1bc0f89c01167be95b9
HP <HP@DESKTOP-8EO1B9C>
2020-11-09 20:20:24 +0300

    feat: add main script

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,8 +1,35 @@
-import './App.css';
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import {
+  Form, BoxContainer, Box, store,
+} from './components';
 
 export function App() {
+  const [buttonText, setButtonText] = useState('Find');
+  const [filteredData, setFilteredData] = useState(store);
+
+  const showFilteredData = useCallback((text) => {
+    setFilteredData(store.filter((item) => item.includes(text)));
+
+    if (filteredData.length) {
+      setButtonText('Find');
+    } else {
+      setButtonText('Add');
+    }
+  }, [filteredData]);
+
+  const addBoxFromInput = useCallback((word) => {
+    store.push(word);
+    setFilteredData(store);
+  }, []);
+
   return (
-    <div className="App" />
+    <div>
+      <Form buttonText={buttonText} onChange={showFilteredData} onSubmit={addBoxFromInput} />
+      <BoxContainer>
+        {
+          filteredData.map((item) => <Box key={item}>{item}</Box>)
+        }
+      </BoxContainer>
+    </div>
   );
 }
